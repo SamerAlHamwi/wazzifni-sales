553fffc3b1fb1bf1bf3fd9d5f10f4fd6494c0d84
rafahdakakni <rafah.dakakni@gmail.com>
2026-03-14 03:31:41 +0300

    deploy

--- backend/models/Rep.js
+++ backend/models/Rep.js
@@ -20,4 +20,17 @@ const repSchema = new mongoose.Schema({
   }
 });
 
-module.exports = mongoose.model('Rep', repSchema);
+const Rep = mongoose.model('Rep', repSchema);
+
+// Attempt to drop the stale 'name_1' index if it exists in the database.
+// This is necessary because we removed the 'name' field but the unique index remains.
+Rep.collection.dropIndex('name_1')
+  .then(() => console.log('Successfully dropped stale name_1 index'))
+  .catch(err => {
+    // IndexNotFound (code 27) is expected if it's already gone
+    if (err.code !== 27) {
+      // console.error('Note: name_1 index could not be dropped (it may not exist)');
+    }
+  });
+
+module.exports = Rep;
